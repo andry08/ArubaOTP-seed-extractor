a04482e4f414493490d1620e058a8f59bed8b0be
Simone Brunello <simone36050@gmail.com>
2021-01-26 15:54:50 +0100

    Implement command-line commands

---
+++ scripts/main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import argparse
+import re as regex
+import traceback
+
+# local import
+import request as extractor
+import genqr
+import totptest
+
+def filter_activation_code(value):
+    # This check was copied from Aruba's app, only basic validation here,
+    # but the code itself has a validation rule too
+    pat = regex.compile('^[2389][0-9]{17}$')
+    if not pat.match(value):
+        raise argparse.ArgumentTypeError('Invalid format')
+    return value
+
+def extract(activation_code, only_output, show_qr):
+    seed = extractor.extract_otp(activation_code)
+    print('Your seed is: {}'.format(seed))
+
+    if not only_output:
+        extractor.write_seed_file(seed)
+
+    if show_qr:
+        print_qr(show_qr)
+
+def generate(seed, time):
+    code = totptest.generate(seed, time)
+    print('Your code is: {}'.format(code))
+
+def print_qr(seed):
+    genqr.generate_and_print(seed)
+        
+
+def main():
+    # parsing argument
+    parser = argparse.ArgumentParser(description='This is a tool to extract ArubaOTP seed and generate OTP codes')
+
+    # first sub command
+    subparser = parser.add_subparsers(title='option', required=True, dest='option',
+                                      description='What do you want to do?')
+
+    # execute command
+    execute_parser = subparser.add_parser('extract', help='Extract ArubaOTP seed')
+
+    execute_parser.add_argument('activation_code', type=filter_activation_code, 
+                                help='Activation code (the long number listed over the qr code)')
+    execute_parser.add_argument('-o', '--only-output', action='store_true',
+                                help='Do not save the OTP seed in the seed.txt file')
+    execute_parser.add_argument('-q', '--show-qr', action='store_true',
+                                help='Show a QR readable by OTP apps')
+
+    # printqr command
+    printqr_parser = subparser.add_parser('printqr', 
+                                          help='Print a QR representation of the OTP seed for OTP apps')
+    
+    printqr_parser.add_argument('-s', '--seed', type=str,
+                                help='Use seed in the paramter')
+
+    # generate command
+    generate_parser = subparser.add_parser('generate', help='Generate OTP by seed')
+
+    generate_parser.add_argument('-s', '--seed', type=str,
+                                 help='Use seed in the paramter')
+    generate_parser.add_argument('-t', '--time', type=int,
+                                 help='Generate OTP in a precise time (UnixEpoch time)')
+
+    # parsing
+    args = parser.parse_args()
+    
+    try:
+        if   args.option == 'extract':
+            extract(args.activation_code, args.only_output, args.show_qr)
+        elif args.option == 'printqr':
+            print_qr(args.seed)
+        elif args.option == 'generate':
+            generate(args.seed, args.time)
+    except:
+        traceback.print_exc()
+
+if __name__ == '__main__':
+    main()
--- scripts/totptest.py
+++ scripts/totptest.py
@@ -2,9 +2,21 @@ import pyotp
 import hashlib
 import base64
 import CBCUtil
+import os
 
-key = base64.b32encode('your-seed-here')
+def generate(seed=None, time=None):
+    if seed is None:
+        try:
+            with open(os.path.join(os.sys.path[0], 'seed.txt'), 'r') as f:
+                seed = f.readline()
+        except Exception as e:
+            raise Exception('Error while reading seed file, make sure to execute the request.py script before this') from e
 
-gen = pyotp.TOTP(key, digits=8, digest=hashlib.sha256)
+    gen = pyotp.TOTP(seed, digits=8, digest=hashlib.sha256)
+    code = 0
+    if time is None:
+        code = gen.now()
+    else:
+        code = gen.at(time)
 
-print(gen.now())
+    return code
